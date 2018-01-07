30ce3b5dcdd9f0f1332173ccc7c401c069cd0d47
Rob Brentnall <rob.brentnall@gmail.com>
2018-01-07 12:00:49 +0000

    commented out call to marvel service search func to allow to compile and imported forms module to fix typeahead error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap/typeahead/typeahead.module';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+
+// Routing & routing components
+import { AppRoutingModule, routingComponents } from './app.routing';
+
+// Components
 import { SearchBoxComponent } from './search-box/search-box.component';
 
+// Services
 import { MarvelService } from './services/marvel.service';
-import { AppRoutingModule, routingComponents } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -18,6 +24,7 @@ import { AppRoutingModule, routingComponents } from './app.routing';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     NgbModule.forRoot(),
     NgbTypeaheadModule
