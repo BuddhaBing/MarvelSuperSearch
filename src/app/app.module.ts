67a341bf589656884db0e723396e267d4038d765
Rob Brentnall <rob.brentnall@gmail.com>
2018-01-07 11:40:12 +0000

    routing module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap/typeahead/typeahead.module';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { DetailsComponent } from './details/details.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 
+import { MarvelService } from './services/marvel.service';
+import { AppRoutingModule, routingComponents } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent,
-    DetailsComponent,
+    routingComponents,
     SearchBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    NgbTypeaheadModule
   ],
-  providers: [],
+  providers: [
+    MarvelService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
